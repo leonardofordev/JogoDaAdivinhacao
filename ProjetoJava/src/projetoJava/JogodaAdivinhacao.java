d7f8fea6083ee6840aa3fac579821c5621eaa988
Leonardo Neto <leonardosouza.batista21@gmail.com>
2024-07-02 08:28:43 -0400

    Add files via upload

---
+++ ProjetoJava/src/projetoJava/JogodaAdivinhacao.java
@@ -0,0 +1,72 @@
+package projetoJava;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogodaAdivinhacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner entrada = new Scanner(System.in);
+	        int continuar = 0;
+	        int tentativa;
+	        int numero;
+	        int numeroAleatorio;
+	        
+	        do {
+	        	
+	        	
+	        	System.out.println("Seja bem vindo ao jogo da adivinhação =D");
+	        	System.out.println("====Como jogar====");
+	        	System.out.println("Você deve adivinhar (dããã) o número que eu estou pensando");
+	        	System.out.println("Caso queira sair basta digitar '0'");
+	        	
+	        	Random randomNumero = new Random();
+	        	numeroAleatorio = randomNumero.nextInt(101);
+	        	System.out.println("Vamos Começar......");
+	        	tentativa = 0;
+	        	
+	        	do {
+	        		tentativa++;
+	        		System.out.println("Digite um número: ");
+	        		numero = entrada.nextInt();
+	        		
+	        		if(numero > numeroAleatorio) {
+	        			System.out.println("O número aleatório é menor;");
+	        			System.out.println("Quantidade de tentativas restantes: " + tentativa);
+	        		}
+	        		else if(numero < numeroAleatorio) {
+	        			System.out.println("O número aleatorio é maior");
+	        			System.out.println("Quantidade de tentativas restantes: " + tentativa);
+	        		}
+	        		
+	        		else {
+	        			System.out.println("Você acertouuu Parabéns");
+	        			System.out.println("Em um total de " + tentativa +" Tentativas");
+	        		}
+	        		
+	        		
+	        	}while(tentativa < 10 && numero != numeroAleatorio );
+	        	
+	        	 if(numero != numeroAleatorio) {
+	                 System.out.println("Você perdeu! Eu sou o melhor! Muhahaha =D");
+	             }
+	             
+	             System.out.println("Deseja jogar novamente? Digite 1 para continuar ou 0 para sair.");
+	             continuar = entrada.nextInt();
+	        		
+	        	
+	        	
+
+	        	
+	        }while(continuar != 0);
+
+	        entrada.close();
+	    }
+	    
+	
+
+
+	}
+
+
